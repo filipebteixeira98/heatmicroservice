8cc02b8aeb322c65f55f4a994d8c34e38c29462d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-10-24 21:51:54 -0300

    feat: created message route/view & error handling

--- lib/heat_tags_web/controllers/messages_controller.ex
+++ lib/heat_tags_web/controllers/messages_controller.ex
@@ -1,10 +1,25 @@
 defmodule HeatTagsWeb.MessagesController do
   use HeatTagsWeb, :controller
 
-  def create(connection, params) do
-    IO.inspect(params)
+  alias HeatTags.Message
+  alias HeatTags.Messages.Create
 
+  def create(connection, params) do
+    params
+    |> Create.call()
+    |> handle_create(connection)
+  end
+
+  defp handle_create({:ok, %Message{} = message}, connection) do
     connection
-    |> text("Received POST request")
+    |> put_status(:created)
+    |> render("create.json", message: message)
+  end
+
+  defp handle_create({:error, %{result: result, status: status}}, connection) do
+    connection
+    |> put_status(status)
+    |> put_view(HeatTagsWeb.ErrorView)
+    |> render("error.json", result: result)
   end
 end
