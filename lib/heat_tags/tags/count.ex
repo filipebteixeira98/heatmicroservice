5ad888f666218366e48153e7ee3e9e0bbb2045bb
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-10-24 23:07:22 -0300

    refactor: improvement of functional statement

--- lib/heat_tags/tags/count.ex
+++ lib/heat_tags/tags/count.ex
@@ -2,15 +2,16 @@ defmodule HeatTags.Tags.Count do
   alias HeatTags.Messages.Get
 
   def call do
-    messages = Get.today_messages()
-
-    Task.async_stream(messages, fn item ->
-      item.message
-      |> String.split()
-      |> Enum.frequencies()
-    end)
+    Get.today_messages()
+    |> Task.async_stream(&count_words(&1.message))
     # |> Enum.to_list()
     |> Enum.reduce(%{}, fn element, accumulator -> sum_values(element, accumulator) end)
+  end
+
+  defp count_words(message) do
+    message
+    |> String.split()
+    |> Enum.frequencies()
   end
 
   defp sum_values({:ok, map}, other_map) do
